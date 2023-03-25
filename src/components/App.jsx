6554a77f06b3af78b9f82cc4a24131129e826a37
Andrii Popov <popov.andrey80@gmail.com>
2023-03-25 13:05:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,6 +29,7 @@ export class App extends Component {
     this.setState((prevState) => { return { users: [...prevState.users, newUser]}})
   }
 
+<<<<<<< Updated upstream
   closeForm= () => {
     this.setState({ isFormShown: false })
   }
@@ -52,3 +53,106 @@ export class App extends Component {
     );
   }
 }
+=======
+    if (!isListShow) {
+      setMovies([]);
+      setPage(1);
+    }
+  }, [isListShow, page]);
+
+  const showList = () => {
+    setIsListShow(prevState => !prevState);
+  };
+
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  const deleteItem = id => {
+    setMovies(prevState => [prevState.filter(item => item.id !== id)]);
+  };
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      <Button text={isListShow ? 'hide List' : 'show List'} clickHandler={showList} />
+      <List movies={movies} deleteMovie={deleteItem} />
+      {movies.length !== 0 && <Button text={'load more...'} clickHandler={loadMore} />}
+    </div>
+  );
+};
+
+// ===================================================================
+
+// import { Component } from 'react';
+
+// import { Button } from './Button/Button';
+// import { fetchMovies } from '../services/moviesApi';
+// import { List } from './List/List';
+// export class App extends Component {
+//   state = {
+//     isListShow: false,
+//     page: 1,
+//     movies: [],
+//     isLoading: false,
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     const { isListShow, page } = this.state;
+
+//     if ((prevState.isListShow !== isListShow || prevState.page !== page) && isListShow) {
+//       this.queryMovies();
+//     }
+
+//     if (prevState.isListShow !== isListShow && isListShow === false) {
+//       this.setState({
+//         page: 1,
+//         movies: [],
+//       });
+//     }
+//   }
+
+//   queryMovies = () => {
+//     this.setState({ isLoading: true });
+//     fetchMovies(this.state.page)
+//       .then(response => {
+//         this.setState(prevState => ({
+//           movies: [...prevState.movies, ...response.data.results],
+//         }));
+//       })
+//       .catch(error => {
+//         console.log('error :>> ', error);
+//       })
+//       .finally(() => {
+//         this.setState({ isLoading: false });
+//       });
+//   };
+
+//   showList = () => {
+//     this.setState(prevState => ({ isListShow: !prevState.isListShow }));
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   delete = id => {
+//     this.setState(prevState => ({
+//       movies: prevState.movies.filter(item => item.id !== id),
+//     }));
+//   };
+
+//   render() {
+//     const { isListShow, movies } = this.state;
+//     return (
+//       <div>
+//         <Button text={!isListShow ? 'show list' : 'hide List'} clickHandler={this.showList} />
+//         <List movies={movies} deleteMovie={this.delete} />
+//         {this.state.movies.length !== 0 && (
+//           <Button text={'load more...'} clickHandler={this.loadMore} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+>>>>>>> Stashed changes
